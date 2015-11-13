8ac84bc2798325188875eedff40d2d528ce36ac2
John Leszczynski <jrleszczynski@gmail.com>
2015-11-13 11:46:33 -0500

    remove unused fs

--- learnyounode/http-collect.js
+++ learnyounode/http-collect.js
@@ -1,6 +1,5 @@
-var fs = require('fs'),
-  http = require('http'),
-concat = require('concat-stream')
+var http = require('http'),
+  concat = require('concat-stream')
 
 var url = process.argv[2]
 
