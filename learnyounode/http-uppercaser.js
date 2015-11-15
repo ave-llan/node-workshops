a77ca56c7a418cdf6784aa34f3b7ce9caa8179b0
John Leszczynski <jrleszczynski@gmail.com>
2015-11-15 08:10:06 -0500

    add server that listens to post requests

---
+++ learnyounode/http-uppercaser.js
@@ -0,0 +1,16 @@
+var map = require('through2-map'),
+   http = require('http')
+
+var port = Number(process.argv[2])
+
+var server = http.createServer(function (request, response) {
+  if (request.method != 'POST')
+    return response.end('send me a POST\n')
+
+  request.pipe(map(function (chunk) {
+    return chunk.toString().toUpperCase()
+  })).pipe(response)
+
+})
+
+server.listen(port)
